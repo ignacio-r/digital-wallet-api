5af1a2e3331caa9a9b5c86fcf3bea89e9662d439
Lucas Wojciechow <lucas.wojciechow@10pines.com>
2019-10-03 20:23:57 -0300

    renombrando DigitWalletApi a DigitalWalletController - eliminando try/catch de login, register y transfer - agregando cashin - corrigiendo nombres de test

--- src/main/kotlin/wallet/DigitalWalletController.kt
+++ src/main/kotlin/wallet/DigitalWalletController.kt
@@ -5,38 +5,39 @@ import io.javalin.http.BadRequestResponse
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.memberProperties
 
-class DigitwalletApi(private val port: Int) {
+class DigitalWalletController(private val port: Int) {
 
     fun checkAllParams(asd: Any): Boolean {
         val props = asd::class.memberProperties as List<KProperty1<Any, *>>
         return props.all { prop -> prop.get(asd).toString().trim() != "" }
     }
-    fun init(): Javalin{
-        val app = Javalin.create{
 
-        }.exception(Exception::class.java) { e, ctx ->
-            e.printStackTrace()
-            ctx.status(500)
-            ctx.json("Error fatal")}
+    fun init(): Javalin {
+        val app = Javalin.create {
+        }
+            .exception(Exception::class.java) { e, ctx ->
+                e.printStackTrace()
+                ctx.status(500)
+                ctx.json("Error fatal")
+            }
+            .exception(BadRequestResponse::class.java) { e, ctx ->
+                ctx.status(400)
+                ctx.result("Bad Request")
+            }
             .start(port)
 
         val service = DigitalWalletService()
 
-        app.exception(BadRequestResponse::class.java) { e, ctx ->
-            ctx.status(400)
-            ctx.result("Bad Request")
-        }
-
         app.post("login") { ctx ->
-            //Falta encriptar la password y el return esta medio falopa
+            //Falta encriptar la password
             val loginWrapper: LoginWrapper = ctx.bodyValidator<LoginWrapper>()
                 .check({ checkAllParams(it)})
                 .get()
-            val userWrapper = service.login(loginWrapper)
-            if (userWrapper != null) {
+            try {
+                service.login(loginWrapper)
                 ctx.status(200)
                 ctx.result("Exito!")
-            } else {
+            } catch (e: LoginException) {
                 ctx.status(401)
                 ctx.result("Usuario o contraseña incorrectos")
             }
@@ -47,11 +48,11 @@ class DigitwalletApi(private val port: Int) {
             registerWrapper = ctx.bodyValidator<RegisterWrapper>()
                 .check({ checkAllParams(it) })
                 .get()
-            val nuevoUsuario = service.register(registerWrapper)
-            if (nuevoUsuario != null) {
+            try {
+                service.register(registerWrapper)
                 ctx.status(200)
                 ctx.result("Registro exitoso")
-            } else {
+            } catch (e: Error) {
                 ctx.status(500)
                 ctx.result("Error de registro. Por favor intente otra vez.")
             }
@@ -59,15 +60,16 @@ class DigitwalletApi(private val port: Int) {
 
         app.post("transfer") { ctx ->
             val transferWrapper: TransferWrapper = ctx.body<TransferWrapper>()
-            val nuevaTransferencia = service.transfer(transferWrapper)
-            if(transferWrapper.amount.toInt() === 0){
+            if (transferWrapper.amount.toInt() <= 0) {
                 ctx.status(400)
                 ctx.result("Las transferencias tienen que tener un monto mayor a cero")
+                return@post
             }
-            else if (nuevaTransferencia) {
+            try {
+                val nuevaTransferencia = service.transfer(transferWrapper)
                 ctx.status(200)
                 ctx.json(transferWrapper.fromCVU)
-            } else {
+            } catch (e: NoSuchElementException) {
                 ctx.status(400)
                 ctx.result("Transferencia fallida, chequear que el CVU destinatario o emisor sean correctos")
             }
@@ -75,8 +77,9 @@ class DigitwalletApi(private val port: Int) {
 
         app.post("/cashin") { ctx ->
             val cashInWrapper: CashInWrapper = ctx.body<CashInWrapper>()
-            val cashIn = service.cashin(cashInWrapper)
-
+            service.cashin(cashInWrapper)
+            ctx.status(200)
+            ctx.result("Cash In exitoso")
         }
 
         app.get("/transaccions/:cvu") { ctx ->
--- src/test/kotlin/wallet/DigitalWalletControllerTest.kt
+++ src/test/kotlin/wallet/DigitalWalletControllerTest.kt
@@ -3,8 +3,9 @@ package wallet
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.FuelManager
 import io.javalin.Javalin
-import junit.framework.Assert.assertEquals
 import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
@@ -13,7 +14,7 @@ class DigitalWalletApiTest {
 
     @BeforeAll
     fun setUp() {
-        api = DigitwalletApi(8000).init()
+        api = DigitalWalletController(8000).init()
         FuelManager.instance.basePath = "http://localhost:${api.port()}/"
     }
     @AfterAll
@@ -23,7 +24,7 @@ class DigitalWalletApiTest {
 
     @Test
     @Order(1)
-    fun unPostNoPuedeTenerElEmailOPasswordSinCompletar(){
+    fun se_retorna_400_al_intentar_loguearse_con_datos_vacios(){
         val json = """{"email":"", "password":"password"}""".trimMargin()
         val(_, response, _) = Fuel.post("login").body(json).response()
 
@@ -34,7 +35,7 @@ class DigitalWalletApiTest {
 
     @Test
     @Order(2)
-    fun unPostConEmailYPasswordNoRegistradosNoIngresaALaAplicacion(){
+    fun se_retorna_400_al_intentar_loguearse_con_datos_erroneos(){
         val json = """{"email":"email", "password":"password"}""".trimMargin()
         val(_, response, _) = Fuel.post("login").body(json).response()
 
@@ -45,7 +46,7 @@ class DigitalWalletApiTest {
 
     @Test
     @Order(3)
-    fun postExitoso(){
+    fun se_retorna_200_al_loguerase_exitosamente(){
         val json = """{"email":"a@gmail.com", "password":"a"}""".trimMargin()
         val(_, response, _) = Fuel.post("login").body(json).response()
 
@@ -55,7 +56,7 @@ class DigitalWalletApiTest {
 
     @Test
     @Order(4)
-    fun siNoEstanCompletosTodosLosCamposDelRegisterNoSeComletaElRegister(){
+    fun se_retorna_400_al_intentar_registrarse_con_datos_incompletos(){
         val json = """{
                         "email":"",
                         "firstName":" ",
@@ -72,7 +73,7 @@ class DigitalWalletApiTest {
 
     @Test
     @Order(5)
-    fun siEstanTodosLosCamposSeRealizaElRegiser(){
+    fun se_retorna_200_al_registrarse_exitosamente(){
       val json = """{
                 "email":"polo@gmail.com",
                 "firstName":"facundo",
@@ -81,14 +82,14 @@ class DigitalWalletApiTest {
                 "password":"polo"
 	
             } """.trimMargin()
-        val(_, response, result) = Fuel.post("register").body(json).response()
+        val(_, response, _) = Fuel.post("register").body(json).response()
         assertEquals(200, response.statusCode)
         assertEquals("OK", response.responseMessage)
 
     }
     @Test
     @Order(6)
-    fun unaTransferenciaNoPuedeSerRealizadaDesdeUnCVUQueNoPertenezcaAUnUsuario(){
+    fun se_retorna_400_al_intentar_realizar_una_transferencia_desde_un_cvu_que_no_pertenece_al_usuario(){
         val json = """{
                 "fromCVU":"01",
                 "toCVU":"519264035",
@@ -102,7 +103,7 @@ class DigitalWalletApiTest {
     }
     @Test
     @Order(8)
-    fun unCVUDestinatarioTambienTieneQuePertenecerAUnUsuarioDelSistema(){
+    fun se_retorna_400_si_un_cvu_de_una_transferencia_no_pertenece_a_ningun_usuario_del_sistema(){
         val json = """{
                 "fromCVU":"060065243",
                 "toCVU":"a",
@@ -116,7 +117,7 @@ class DigitalWalletApiTest {
 
     @Test
     @Order(9)
-    fun transferExitoso(){
+    fun se_retorna_400_si_el_monto_de_la_transferencia_es_0(){
         val json = """{
                 "fromCVU":"060065243",
                 "toCVU":"519264035",
@@ -127,23 +128,50 @@ class DigitalWalletApiTest {
         assertEquals(400, response.statusCode)
         assertEquals("Las transferencias tienen que tener un monto mayor a cero", String(response.data))
     }
+
     @Test
     @Order(10)
+    fun se_retorna_400_si_el_monto_de_la_transferencia_es_menor_a_0(){
+        val json = """{
+                "fromCVU":"060065243",
+                "toCVU":"519264035",
+                "amount":"-1"
+            } """.trimMargin()
+        val(_, response, _) = Fuel.post("transfer").body(json).response()
+
+        assertEquals(400, response.statusCode)
+        assertEquals("Las transferencias tienen que tener un monto mayor a cero", String(response.data))
+    }
+
+//    @Test
+//    @Order(10)
+//    fun se_retorna_200_si_la_transferencia_es_exitosa(){
+//        val json = """{
+//                "fromCVU":"060065243",
+//                "toCVU":"519264035",
+//                "amount":"1"
+//            } """.trimMargin()
+//        val(_, response, _) = Fuel.post("transfer").body(json).response()
+//
+//        assertEquals(200, response.statusCode)
+//    }
+    @Test
+    @Order(11)
     fun cashIn(){
         //algo de cashIn
     }
     @Test
-    @Order(11)
+    @Order(12)
     fun transactions() {
     }
 
     @Test
-    @Order(12)
+    @Order(13)
     fun delete() {
     }
 
     @Test
-    @Order(13)
+    @Order(14)
     fun sePideElBalanceDeUnaCuentaPorSuCVU() {
         val (_, response, _) = Fuel.get("account/060065243").response()
         assertEquals("amount: 0.0", String(response.data).toString())
