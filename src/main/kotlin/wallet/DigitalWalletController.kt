0f2fcb40308dca90e11453bb50bb39fa8390a9f3
pache0015 <pache.facundo@gmail.com>
2019-10-31 20:23:50 -0300

    it.enableCorsForAllOrigins() en javalin.create

--- src/main/kotlin/wallet/DigitalWalletController.kt
+++ src/main/kotlin/wallet/DigitalWalletController.kt
@@ -14,6 +14,7 @@ class DigitalWalletController(private val port: Int) {
 
     fun init(): Javalin {
         val app = Javalin.create {
+            it.enableCorsForAllOrigins()
         }
             .exception(Exception::class.java) { e, ctx ->
                 e.printStackTrace()
